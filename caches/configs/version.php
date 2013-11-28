d51928ffcbdc2ccf4a3dd8cc4e7c1ab69ae027ea
yiilover <yiilover@gmail.com>
2013-11-29 01:14:59 +0800

    init

---
+++ caches/configs/version.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+'pc_version' => 'V9.5.1',	//phpcms 版本号
+'pc_release' => '20131127',	//phpcms 更新日期
+);
+?>
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://localhost:7014/',
+    'authkey' => '5Ee4LB0rpyhoCzygfzL8g0UVVNhlruAB',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
